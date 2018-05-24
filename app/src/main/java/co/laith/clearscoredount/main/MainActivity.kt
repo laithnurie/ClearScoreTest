c14caeb70debbad164a8a5241502d58c59dc84f9
Laith <laithnurie@gmail.com>
2018-05-24 20:12:34 +0100

    main view model injected

--- app/src/main/java/co/laith/clearscoredount/main/MainActivity.kt
+++ app/src/main/java/co/laith/clearscoredount/main/MainActivity.kt
@@ -5,19 +5,17 @@ import android.support.v7.app.AppCompatActivity
 import co.laith.clearscoredount.R
 import co.laith.clearscoredount.di.DaggerMainComponent
 import co.laith.clearscoredount.donut.DountView
-import co.laith.clearscoredount.service.ClearScoreService
-import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var scoreDount: DountView
-    private lateinit var viewModel: MainViewModel
 
-    private var disposable: Disposable? = null
+    private val disposables = CompositeDisposable()
 
     @Inject
-    lateinit var clearScoreService: ClearScoreService
+    lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +24,6 @@ class MainActivity : AppCompatActivity() {
 
         scoreDount = findViewById(R.id.score_dount)
 
-        viewModel = MainViewModel(clearScoreService)
     }
 
     override fun onResume() {
@@ -36,20 +33,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        if (!disposable?.isDisposed!!) {
-            disposable?.dispose()
-        }
+        disposables.clear()
     }
 
     private fun fetchScore() {
-        if (disposable != null && !disposable?.isDisposed!!) {
-            disposable?.dispose()
-        }
-        disposable = viewModel.getCreditScore()
+        disposables.add(viewModel.getCreditScore()
                 .subscribe({
                     scoreDount.setScore(it.score, it.minLimit, it.maxLimit)
                 }, {
                     scoreDount.showError(it)
-                })
+                }))
     }
 }
--- app/src/main/java/co/laith/clearscoredount/main/MainViewModel.kt
+++ app/src/main/java/co/laith/clearscoredount/main/MainViewModel.kt
@@ -2,8 +2,9 @@ package co.laith.clearscoredount.main
 
 import co.laith.clearscoredount.service.ClearScoreService
 import io.reactivex.Single
+import javax.inject.Inject
 
-class MainViewModel(private val clearScoreService: ClearScoreService) {
+class MainViewModel @Inject constructor(private val clearScoreService: ClearScoreService) {
 
     fun getCreditScore(): Single<MainUiModel> {
         return clearScoreService.getCrediScore().map {
